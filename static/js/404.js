ef249f5b1d50f37e5dad67fa8aa174adb0632932
Christophe HERAL <christophe.heral@gmail.com>
2018-10-24 23:25:37 +0200

    Correction page 404

--- static/js/404.js
+++ static/js/404.js
@@ -1,8 +1,6 @@
 jQuery(document).ready(function () {
     jQuery('body').addClass('sub-page');
     theme.init();
-    theme.initSponsorsSilverSlider();
-    theme.initPartenaireSlider();
 });
 
 jQuery(window).load(function () {
