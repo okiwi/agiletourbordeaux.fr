2ee54959f14b73442a976da3e8e1adce9c1c92c9
Sam Cranford <me@nostradamnit.com>
2019-08-12 15:44:25 +0200

    Ajoute OSM plugin gesture-handling pour mieux gerer le scroll sur le map

--- static/js/osm-map.js
+++ static/js/osm-map.js
@@ -2,13 +2,17 @@ var latitudeIUT = 44.83061;
 var longitudeIUT = -0.56216;
 var carteAtbdxCentreeSurIUT = null;
 function initMap() {
-    carteAtbdxCentreeSurIUT = L.map('map').setView([latitudeIUT, longitudeIUT], 16);
+    carteAtbdxCentreeSurIUT = L.map('map', {gestureHandling: true}).setView([latitudeIUT, longitudeIUT], 16);
     // Leaflet ne récupère pas les cartes (tiles) sur un serveur par défaut. Nous devons lui préciser où nous souhaitons les récupérer. Ici, openstreetmap.fr
     L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
         // Il est toujours bien de laisser le lien vers la source des données
         attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
         minZoom: 5,
         maxZoom: 20
+    }).addTo(carteAtbdxCentreeSurIUT);
+    // Add tile layer
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(carteAtbdxCentreeSurIUT);
     var marker = L.marker([latitudeIUT, longitudeIUT]).addTo(carteAtbdxCentreeSurIUT);
     L.control.scale().addTo(carteAtbdxCentreeSurIUT);
