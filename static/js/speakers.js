cd5209077de7060544bfc9c2089c39b7eda591e1
dav-33 <david.couplez@free.fr>
2022-09-16 16:59:19 -0400

    Programme et speakers

--- static/js/speakers.js
+++ static/js/speakers.js
@@ -2,6 +2,6 @@ jQuery(document).ready(function () {
     jQuery('body').addClass('sub-page');
     theme.init();
 
-    // add active class to blog menu
-    jQuery('ul.sf-menu li:first-child').addClass('active');
+    // add active class to speaker menu
+    jQuery('ul.sf-menu li:nth-child(2)').addClass('active');
 });
--- static/js/sponsors.js
+++ static/js/sponsors.js
@@ -2,6 +2,6 @@ jQuery(document).ready(function () {
     jQuery('body').addClass('sub-page');
     theme.init();
 
-    // add active class to blog menu
-    jQuery('ul.sf-menu li:first-child').addClass('active');
+    // add active class to sponsors menu
+    jQuery('ul.sf-menu li:nth-child(3)').addClass('active');
 });
